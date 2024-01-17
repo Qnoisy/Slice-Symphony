c1ee1524f580780698bbe84a2bc040112dead497
Aveskulov <WenJoymk3@gmail.com>
2024-01-17 22:33:21 +0100

    add cards and other files

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -1,5 +1,31 @@
+import '../common/general.scss';
+import '../common/let.scss';
+import '../common/reset.scss';
+import { Container } from './Container';
+import { Footer } from './Footer';
+import { Header } from './headerComponents/Header';
+import { Main } from './mainComponents/Main';
+
 const Wrapper = () => {
-	return <div>Test</div>;
+	return (
+		<div className='wrapper'>
+			<div className='header'>
+				<Container>
+					<Header />
+				</Container>
+			</div>
+			<div className='main'>
+				<Container>
+					<Main />
+				</Container>
+			</div>
+			<div className='footer'>
+				<Container>
+					<Footer />
+				</Container>
+			</div>
+		</div>
+	);
 };
 
 export default Wrapper;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import Wrapper from './components/wrapper';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
-root.render(<Wrapper />);
+root.render(
+	<BrowserRouter>
+		<Wrapper />
+	</BrowserRouter>
+);
